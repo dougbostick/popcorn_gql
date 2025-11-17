d7e63c9db73322e9a7cf399fbba3b55bba4aee15
Doug Bostick <dbostick@averoinc.com>
2025-11-17 13:08:27 -0500

    added dotenv for .env file, added profile links for friends list cards, mongo db cluster deployed and ready for prd

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import { Layout } from './components/Layout';
 import { MainFeed } from './components/MainFeed';
 import { Profile } from './components/Profile';
 import { Friends } from './components/Friends';
+import { UserProfile } from './components/UserProfile';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
               <Route path="/" element={<MainFeed />} />
               <Route path="/profile" element={<Profile />} />
               <Route path="/friends" element={<Friends />} />
+              <Route path="/user/:userId" element={<UserProfile />} />
             </Routes>
           </Layout>
         </ProtectedRoute>
